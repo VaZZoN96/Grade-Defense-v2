006a1cbb6f2968865313a363922966c16d79e7be
Damian <18752953+VaZZoN96@users.noreply.github.com>
2019-05-29 23:11:30 +0200

    Added mouse handling, created two scenes (main menu and battle)

--- ui.cpp
+++ ui.cpp
@@ -5,12 +5,108 @@ UI::UI()
 
 }
 
+void UI::mouseHandling(float x, float y)
+{
+	button_pushed_ = 1;
+	x_button_ = x;
+	y_button_ = y;
+}
+
+void UI::reset_mouse_button_pushed()
+{
+	button_pushed_ = 0;
+}
+
+int UI::get_mouse_button_pushed()
+{
+	return button_pushed_;
+}
+
+sf::Vector2f UI::get_mouse_cords()
+{
+	return sf::Vector2f(x_button_, y_button_);
+}
+
+void UI::main_menu()
+{
+	buttons_main_.emplace_back(std::make_unique<Button>(400, 183, 400, 100, new_game));
+	buttons_main_.emplace_back(std::make_unique<Button>(400, 417, 400, 100, options));
+}
+
 void UI::tower_menu()
 {
 
 }
 
+void UI::tower_menu_show()
+{
+	active_ = tower;
+}
+
 void UI::build_menu()
 {
 
+}
+
+void UI::build_menu_show()
+{
+	active_ = build;
+}
+
+void UI::step(sf::RenderWindow &window)
+{
+	if(active_ == mai)
+	{
+		for(unsigned int i=0; i<buttons_main_.size(); i++)
+		{
+			Button &buttons = dynamic_cast<Button &>(*buttons_main_[i]);
+
+			if(get_mouse_button_pushed() == 1)
+			{
+				if(get_mouse_cords().x >= buttons.getGlobalBounds().left && get_mouse_cords().x <= buttons.getGlobalBounds().left + buttons.getGlobalBounds().width)
+				{
+					if(get_mouse_cords().y >= buttons.getGlobalBounds().top && get_mouse_cords().y <= buttons.getGlobalBounds().top + buttons.getGlobalBounds().height)
+					{
+						if(buttons.get_button_function() == new_game)
+						{
+							scene_ = battle;
+							active_ = none;
+						}
+						else if(buttons.get_button_function() == options)
+						{
+
+						}
+					}
+				}
+			}
+
+			window.draw(buttons);
+		}
+	}
+	else if(active_ == tower)
+	{
+		for(unsigned int i=0; i<buttons_tower_.size(); i++)
+		{
+			Button &buttons = dynamic_cast<Button &>(*buttons_tower_[i]);
+			window.draw(buttons);
+		}
+	}
+	else if(active_ == build)
+	{
+		for(unsigned int i=0; i<buttons_build_.size(); i++)
+		{
+			Button &buttons = dynamic_cast<Button &>(*buttons_build_[i]);
+			window.draw(buttons);
+		}
+	}
+}
+
+Menu UI::get_scene()
+{
+	return scene_;
+}
+
+Menu UI::get_active()
+{
+	return active_;
 }
--- ui.h
+++ ui.h
@@ -5,13 +5,37 @@
 
 #include "tower.h"
 
+#include <iostream>
+
+enum Menu { none, mai, battle, tower, build };
+
 class UI
 {
 public:
 	UI();
-	void tower_menu();
-	void build_menu();
+	void mouseHandling(float x, float y);
+	void reset_mouse_button_pushed();
+	int get_mouse_button_pushed();
+	sf::Vector2f get_mouse_cords();
+
+	void main_menu(); //Prepare main menu
+	void main_menu_show();
+	void tower_menu(); //Prepare tower menu
+	void tower_menu_show();
+	void build_menu(); //Prepare build menu
+	void build_menu_show();
+	void step(sf::RenderWindow &window);
+	Menu get_scene();
+	Menu get_active();
 private:
+	int button_pushed_ = 0;
+	float x_button_ = 0;
+	float y_button_ = 0;
+	Menu scene_ = mai;
+	Menu active_ = mai;
+	std::vector<std::unique_ptr<Button>> buttons_main_;
+	std::vector<std::unique_ptr<Button>> buttons_tower_;
+	std::vector<std::unique_ptr<Button>> buttons_build_;
 };
 
 #endif // UI_H
