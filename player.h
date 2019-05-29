6984439075b551422fd4f4b481dee707930ca427
Damian <18752953+VaZZoN96@users.noreply.github.com>
2019-05-29 23:10:27 +0200

    Changed mouse handling from Player class to UI class

--- player.h
+++ player.h
@@ -9,14 +9,7 @@ class Player
 {
 public:
 	Player();
-	void mouseHandling(float x, float y);
-	void reset_button_pushed();
-	int get_button_pushed();
-	sf::Vector2f get_mouse_cords();
 private:
-	int button_pushed_ = 0;
-	float x_button_ = 0;
-	float y_button_ = 0;
 };
 
 #endif // PLAYER_H
