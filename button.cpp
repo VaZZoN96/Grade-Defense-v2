52c2abba3e050701dd4914b59225f6fc32575bc1
Damian <18752953+VaZZoN96@users.noreply.github.com>
2019-05-29 23:12:37 +0200

    Improved Button class, buttons can't be textured now

--- button.cpp
+++ button.cpp
@@ -1,9 +1,16 @@
 #include "button.h"
 
-Button::Button()
+Button::Button(float x, float y, int width, int height, Button_function func)
+	: sf::RectangleShape(sf::Vector2f(width,height)), x_(x), y_(y), func_(func)
 {
-
+	setPosition(x,y);
+	setFillColor(sf::Color(255,0,0));
 }
 
 Button::~Button()
 {}
+
+Button_function Button::get_button_function()
+{
+	return func_;
+}
--- button.h
+++ button.h
@@ -4,13 +4,19 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-class Button : public sf::Sprite
+enum Button_function { new_game, options };
+
+class Button : public sf::RectangleShape
 {
 public:
-	Button();
+	Button(float x, float y, int width, int height, Button_function func);
 	virtual ~Button();
+	Button_function get_button_function();
 private:
+	float x_;
+	float y_;
 	sf::FloatRect bounds = getGlobalBounds();
+	Button_function func_;
 };
 
 #endif // BUTTON_H
