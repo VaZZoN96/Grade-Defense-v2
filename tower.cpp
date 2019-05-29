2b79f6a8915e1828626d7149a2ebc23e1cb04d06
Damian <18752953+VaZZoN96@users.noreply.github.com>
2019-05-29 23:56:00 +0200

    Removed sketch of tower stats from Unit class and added to Tower class

--- tower.cpp
+++ tower.cpp
@@ -7,3 +7,67 @@ Tower::Tower()
 
 Tower::~Tower()
 {}
+
+/*//Set range, dmg, cost
+	if(type == 1) //Machine gun
+	{
+		rot_ = 0;
+		range_ = 0;
+		dmg_ = 0;
+		cost_ = 0;
+		at_speed_ = 0;
+	}
+	else if(type == 2) //Rocket launcher
+	{
+		rot_ = 0;
+		range_ = 0;
+		dmg_ = 0;
+		cost_ = 0;
+		at_speed_ = 0;
+	}
+	else if(type == 3) //Lightning gun
+	{
+		rot_ = 0;
+		range_ = 0;
+		dmg_ = 0;
+		cost_ = 0;
+		at_speed_ = 0;
+		chains_ = 0;
+	}
+	else if(type == 4) //Laser gun
+	{
+		rot_ = 0;
+		range_ = 0;
+		dmg_ = 0;
+		cost_ = 0;
+		cont_ = 1;
+	}
+	else if(type == 5) //Flamethrower
+	{
+		rot_ = 0;
+		range_ = 0;
+		dmg_ = 0;
+		cost_ = 0;
+		cont_ = 1;
+	}
+	else if(type == 6) //Gauss gun
+	{
+		rot_ = 0;
+		range_ = 0;
+		dmg_ = 0;
+		cost_ = 0;
+		at_speed_ = 0;
+	}
+	else if(type == 7) //Slower
+	{
+		range_ = 0;
+		cost_ = 0;
+		cont_ = 1;
+	}
+	else if(type == 8) //EMP
+	{
+		range_ = 0;
+		dmg_ = 0;
+		cost_ = 0;
+		at_speed_ = 0;
+	}*/
