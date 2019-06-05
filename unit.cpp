de5d192b3626d1eaac9e9250efffb2b5ba469604
Damian <18752953+VaZZoN96@users.noreply.github.com>
2019-06-05 10:12:41 +0200

    Added functions to get unit's data

--- unit.cpp
+++ unit.cpp
@@ -2,5 +2,50 @@
 
 Unit::Unit()
 {
+	if(type_ == normal)
+	{
+		hp_ = 100;
+		speed_ = 0;
+		armor_ = 0;
+	}
+	else if(type_ == fast)
+	{
+		hp_ = 0;
+		speed_ = 0;
+		armor_ = 0;
+	}
+	else if(type_ == tank)
+	{
+		hp_ = 0;
+		speed_ = 0;
+		armor_ = 0;
+	}
+}
 
+Unit::~Unit()
+{}
+
+float Unit::get_hp()
+{
+	return hp_;
+}
+
+float Unit::get_speed()
+{
+	return speed_;
+}
+
+int Unit::get_emp()
+{
+	return emp_;
+}
+
+float Unit::get_slow()
+{
+	return slow_;
+}
+
+float Unit::get_armor()
+{
+	return armor_;
 }
--- unit.h
+++ unit.h
@@ -2,10 +2,25 @@
 #define UNIT_H
 #include "entity.h"
 
+enum Unit_type { normal, fast, tank };
+
 class Unit : public Entity
 {
 public:
 	Unit();
+	virtual ~Unit();
+	float get_hp();
+	float get_speed();
+	int get_emp();
+	float get_slow();
+	float get_armor();
+private:
+	Unit_type type_;
+	float hp_;
+	float speed_;
+	float armor_;
+	int emp_ = 0; //emp flag
+	float slow_ = 0; //slow duration in seconds
 };
 
 #endif // UNIT_H
